664fbbe68b17ed82e8a2783c6672c9530f85cfe3
Siddhartha Pathak <siddhartha.pathak0216@gmail.com>
2024-03-20 15:27:37 +0530

    phase one

---
+++ lib/pages/login.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LogIn extends StatefulWidget {
+  const LogIn({super.key});
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
